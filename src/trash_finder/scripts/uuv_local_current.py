676adffbd04cada12bce4d16eb0ae94038f94949
michellesit <mcsit@eng.ucsd.edu>
2020-02-11 21:05:28 -0800

    new: interpolates u v values based on location

--- src/trash_finder/scripts/uuv_local_current.py
+++ src/trash_finder/scripts/uuv_local_current.py
@@ -3,13 +3,19 @@
 ##get the robot's pose and depth
 ##subscribe to the robot's ground_truth_to_tf_[name of robot]/pose
 import rospy
-from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
+
+from scipy.io import netcdf
+import numpy as np
+from haversine_dist import calc_dist_lat_lon
+from scipy.interpolate import interpn
+
+import pdb
 
 def callback(msg):
 	position = msg.pose.position
 	rospy.loginfo("Point position: [%f, %f, %f]" %(position.x, position.y, position.z))
-	# rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
 
 def listener():
 	rospy.init_node('get_robot_pose', anonymous=True)
@@ -17,10 +23,146 @@ def listener():
 
 	rospy.spin()
 
+
+##Get the current velocity and direction at that depth
+def get_current_data(data_file, uuv_x, uuv_y, uuv_z):
+	##convert the lat-lon info into x,y coordinates
+
+	# data_file[]
+	pass
+
+##TODO: Unit test to make sure this makes sense for all data points (all edge cases)
+##CASE: Data point has no info
+##CASE: None of the data points have info
+##CASE: At the edge of the map
+##CASE: At the very bottom of the ground
+
+def convert_to_xy(loc1, loc2, loc3, loc4, current_file, uuv_pos):
+    ##loc1 to loc2 (width)
+    width = calc_dist_lat_lon(loc1[0], loc1[1], loc2[0], loc2[1])
+    ##loc2 to loc 3 (height)
+    height = calc_dist_lat_lon(loc2[0], loc2[1], loc3[0], loc3[1])
+
+    min_lat = min(loc1[0], loc2[0], loc3[0], loc4[0])
+    max_lat = max(loc1[0], loc2[0], loc3[0], loc4[0])
+    min_lon = min(loc1[1], loc2[1], loc3[1], loc4[1])
+    max_lon = max(loc1[1], loc2[1], loc3[1], loc4[1])
+
+
+    lon = current_file.variables['lon']
+    lon = lon[:] - 360
+    lat = current_file.variables['lat']
+
+    d = current_file.variables['depth']
+    u = current_file.variables['u']
+    v = current_file.variables['v']
+
+    [uuv_x, uuv_y, uuv_z] = uuv_pos
+
+    ##get the idx values from lat, lon arrays
+    lon_idx = np.where((lon[:]>=min_lon) & (lon[:]<=max_lon))[0]
+    lat_idx = np.where((lat[:]>=min_lat) & (lat[:]<=max_lat))[0]
+
+    lon_area = lon[lon_idx]
+    lon_zeroed = lon_area - min(lon[lon_idx])
+    lon_xcoords = lon_zeroed*(width/max(lon_zeroed))*1000
+    print ('lon_xcoords: ', lon_xcoords)
+
+    lat_area = lat[lat_idx]
+    lat_zeroed = lat_area - min(lat[lat_idx])
+    lat_ycoords = lat_zeroed*(height/max(lat_zeroed))*1000
+    print ("lat_ycoords: ", lat_ycoords)
+
+
+    ##This is all the new info
+
+    ##figure out between which idx values the coordinates are in
+    depth_idx_pt = find_idx_boundary(uuv_z, d)
+    lon_idx_pt = find_idx_boundary(uuv_x, lon_xcoords)
+    lat_idx_pt = find_idx_boundary(uuv_y, lat_ycoords)
+
+    print( "depth shape: ", d.shape)
+    print ("lat_idx shape: ", lat_idx.shape)
+    print ("lon_idx shape: ", lon_idx.shape)
+
+    print ("depth_idx_pt: ", depth_idx_pt)
+    print ("lat_idx_pt: ", lat_idx_pt)
+    print ("lon_idx_pt: ", lon_idx_pt)
+
+    ##Use those idx values to get depth values from u and v
+    ##U and V are read-only(?). Hence, copy the values
+
+    print (lat_idx[lat_idx_pt[0]], lat_idx[lat_idx_pt[-1]])
+    print (lon_idx[lon_idx_pt[0]], lon_idx[lon_idx_pt[-1]])
+
+
+
+    u_above = u[:, depth_idx_pt[1]+1, lat_idx[lat_idx_pt[0]]:lat_idx[lat_idx_pt[1]+1], lon_idx[lon_idx_pt[0]]:lon_idx[lon_idx_pt[1]+1]].copy()
+    v_above = v[:, depth_idx_pt[1]+1, lat_idx[lat_idx_pt[0]]:lat_idx[lat_idx_pt[1]+1], lon_idx[lon_idx_pt[0]]:lon_idx[lon_idx_pt[1]+1]].copy()
+
+    u_below = u[:, depth_idx_pt[0], lat_idx[lat_idx_pt[0]]:lat_idx[lat_idx_pt[1]+1], lon_idx[lon_idx_pt[0]]:lon_idx[lon_idx_pt[1]+1]].copy()
+    v_below = v[:, depth_idx_pt[0], lat_idx[lat_idx_pt[0]]:lat_idx[lat_idx_pt[1]+1], lon_idx[lon_idx_pt[0]]:lon_idx[lon_idx_pt[1]+1]].copy()
+
+    print ("u")
+    print (u_above)
+    print (u_below)
+
+    print ("v")
+    print (v_above)
+    print (v_below)
+
+    u_box = np.vstack((u_below, u_above))
+    v_box = np.vstack((v_below, v_above))
+
+    ##3D interpolate the values from the coordinates here
+    ##get the individual u,v coordiantes based off of these things
+    lat_box = np.array([lat_ycoords[lat_idx_pt[0]], lat_ycoords[lat_idx_pt[1]]])
+    lon_box = np.array([lon_xcoords[lon_idx_pt[0]], lon_xcoords[lon_idx_pt[1]]])
+    depth_box = np.array([d[depth_idx_pt[0]], d[depth_idx_pt[1]+1]])
+
+    print ("lat_box: ", lat_box)
+    print ("lon_box: ", lon_box)
+    print ("depth_box: ", depth_box)
+
+    ##Coordinate space, the actual values, the pts you're trying to interpolate
+    uuv_u = interpn((lon_box, lat_box, depth_box), u_box, np.array([uuv_x, uuv_y, uuv_z]).T)
+    uuv_v = interpn((lon_box, lat_box, depth_box), v_box, np.array([uuv_x, uuv_y, uuv_z]).T)
+
+    print ("uuv_u: ", uuv_u)
+    print ("uuv_v: ", uuv_v)
+
+    pdb.set_trace()
+
+    return uuv_u[0], uu_v[0]
+    
+
+
+##Helper method
+def find_idx_boundary(find_value, data_arr):
+	max_idx = np.where(find_value <= data_arr[:])[0]
+	min_idx = np.where(find_value >= data_arr[:])[0]
+
+	return max(min_idx), min(max_idx)
+
 if __name__ == '__main__':
-	listener()
+	current_file = netcdf.NetCDFFile('/home/msit/Desktop/oceans_2020/src/trash_finder/scripts/ca_subCA_das_2020010615.nc')
+
+	##we'll pretend that this info is being fed in:
+	lajolla = [32.838, -117.343]
+	lajolla_20 = [32.838, -117.543]
+	coronado = [32.66, -117.343]
+	coronado_20 = [32.66, -117.543]
+
+	uuv_pos = [5000, 6000, 150]
+	uuv_u, uuv_v = convert_to_xy(lajolla, lajolla_20, coronado, coronado_20, current_file, uuv_pos)
+
+	##send data via ROS
 
 
+
+
+	# listener()
 
 
 ##Get the current velocity and direction at that depth
+
