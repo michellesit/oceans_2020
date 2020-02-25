ffc60b9fd3358f0c92d382a868a348ef4041891b
michellesit <mcsit@eng.ucsd.edu>
2020-02-24 19:11:28 -0800

    WIP added for loop

--- src/trash_finder/scripts/randomize_init_soda_cans.py
+++ src/trash_finder/scripts/randomize_init_soda_cans.py
@@ -14,21 +14,27 @@ sc_pos = [10, 40, 10, 0.5, 1]
 include_file = e.SubElement(launch, "include")
 include_file.set("file", "$(find trash_worlds)/launch/single_soda.launch")
 
-sc_x = e.SubElement(include_file, "arg")
-sc_x.set("default", str(sc_pos[0]))
-sc_x.set("name", "x")
+num_soda_cans = 3
+for i in range (num_soda_cans):
+	sc_x = e.SubElement(include_file, "arg")
+	sc_x.set("default", str(sc_pos[0]))
+	sc_x.set("name", "x")
 
-# sc_y = e.SubElement(include_file, "arg")
-# sc_y.set("name", "y", "default", sc_pos[1])
+	sc_y = e.SubElement(include_file, "arg")
+	sc_y.set("default", str(sc_pos[1]))
+	sc_y.set("name", "y")
 
-# sc_z = e.SubElement(include_file, "arg")
-# sc_z.set("name", "z", "default", sc_pos[2])
+	sc_z = e.SubElement(include_file, "arg")
+	sc_y.set("default", str(sc_pos[2]))
+	sc_z.set("name", "z")
 
-# sc_y = e.SubElement(include_file, "arg")
-# sc_y.set("name", "yaw", "default", sc_pos[3])
+	sc_yaw = e.SubElement(include_file, "arg")
+	sc_y.set("default", str(sc_pos[3]))
+	sc_yaw.set("name", "yaw")
 
-# sc_y = e.SubElement(include_file, "arg")
-# sc_y.set("name", "namespace", "default", sc_pos[4])
+	sc_ns = e.SubElement(include_file, "arg")
+	sc_y.set("default", "soda_can{0}".format(i))
+	sc_ns.set("name", "namespace")
 
 
 ##write the xml file
@@ -36,9 +42,3 @@ sc_x.set("name", "x")
 tree.write("test.launch")
 
 ##Instead of rewriting the whole file, just add to the section at the end
-
-
-# num_soda_cans = 20
-
-# def generate_data_points(self):
-# 	for x in range(num_soda_cans):
