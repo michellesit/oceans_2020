3d0b6c05fef143403e1195d03eeb7be7456c1726
Michelle Sit <mcsit@eng.ucsd.edu>
2020-04-15 03:22:38 -0700

    current block works. TODO test depth block

--- src/trash_utils/src/trash_utils/finder_utils.py
+++ src/trash_utils/src/trash_utils/finder_utils.py
@@ -1,13 +1,15 @@
 from ConfigParser import SafeConfigParser
 
 import numpy as np
-from scipy.interpolate import griddata
+from scipy.interpolate import RegularGridInterpolator, interpn
 from scipy.io import netcdf
 
 import pdb
 import rospkg
 
 from trash_utils.haversine_dist import haversine
+
+import matplotlib.pyplot as plt
 
 '''
 Utility functions to support the trash_finder/scripts files
@@ -94,67 +96,6 @@ def lat_lon_to_xy(bbox, lat, lon):
 	return x,y
 
 
-def find_bounding_block(data, bbox, coord1, coord2):
-	'''
-	Given a current file, find the two bounding values from that file that the coord1 or 
-		coord2 value falls between
-	
-	##TODO: GENERALIZE TO ANY/ALL THINGS
-	'''
-	all_currents = netcdf.NetCDFFile(current_file)
-
-	lat = all_currents.variables['lat'][:].copy()
-	lon = all_currents.variables['lon'][:].copy()
-	lon -= 360
-
-	min_lat = min(bbox[:, 0])
-	max_lat = max(bbox[:, 0])
-	min_lon = min(bbox[:, 1])
-	max_lon = max(bbox[:, 1])
-
-	width = haversine(bbox[0,0], bbox[0,1], bbox[1,0], bbox[1,1])
-	height = haversine(bbox[1,0], bbox[1,1], bbox[2,0], bbox[2,1])
-
-	lon_start = max(np.where(lon<=min_lon)[0])
-	lon_end = min(np.where(lon>=max_lon)[0])
-
-	#lon_start = np.searchsorted(lon, min_lon, 'left')
-	#lon_end = np.searchsorted(lon, max_lon, 'right')
-
-	lat_start = np.searchsorted(lat, min_lat, 'left')
-	lat_end = np.searchsorted(lat, max_lat, 'right')
-
-	if lon_start == lon_end:
-		lon_idx = np.arange(lon_start-1, lon_end+1)
-	else:
-		lon_idx = np.array((lon_start, lon_end))
-	if lat_start == lat_end:
-		lat_idx = np.arange(lat_start-1, lat_end+1)
-	else:
-		lat_idx = np.array((lat_start, lat_end))
-
-	print ("lon_idx: ", lon_idx)
-	print ("lat_idx: ", lat_idx)
-
-	# lon_idx = np.where((lon[:]>=min_lon) & (lon[:]<=max_lon))[0]
-	# lat_idx = np.where((lat[:]>=min_lat) & (lat[:]<=max_lat))[0]
-
-	lon_area = lon[lon_idx]
-	lat_area = lat[lat_idx]
-
-	print ("lon_area: ", lon_area)
-	print ("lat_area: ", lat_area)
-
-	pdb.set_trace()
-
-	# lon_zeroed = lon_area - min(lon_area)
-	# lon_ycoords = lon_zeroed*(height/max(lon_zeroed))*1000
-
-	# lat_zeroed = lat_area - min(lat_area)
-	# lat_xcoords = lat_zeroed*(width/max(lat_zeroed))*1000
-
-	return lat_area, lon_area
-
 def find_bound_idx(data, coord):
 	'''
 	Given a point and data array that may not have the specific point, finds the two values
@@ -190,6 +131,33 @@ def find_bound_idx(data, coord):
 	return np.array((min(start, end), max(start, end)))
 
 
+def grid_data(interp_f, x_bound, y_bound):
+	'''
+	Input:
+		interp_f : Scipy RegularGridInterpolator function that interpolates the data
+			Takes an array of coordinates as input
+		x_bound (list) : list of 
+
+		dim (array): size=(1,2), gives the x,y dimension of the output
+
+	Returns:
+		interp_data (np.array): shape=(dim), interpolated data of the provided coordinates
+
+	'''
+	##meshgrid the data based on dim size
+	##TODO: Figure out how to incorporate x and y bound
+	x = np.arange(x_bound[0], x_bound[1]+1)
+	y = np.arange(y_bound[0], y_bound[1]+1)
+
+	xv, yv = np.meshgrid(x,y)
+	grid = zip(xv.flatten(), yv.flatten())
+
+	##feed it into the interpolation function to get output data of the same size
+	interp_data = interp_f(grid)
+
+	return interp_data.reshape(abs(x_bound[1]-x_bound[0]), abs(y_bound[1] - y_bound[0]))
+
+
 def get_depth_block(bbox, topo_file):
 	'''
 	Input:
@@ -198,21 +166,41 @@ def get_depth_block(bbox, topo_file):
 		topo_file (string): path to netcdf file containing the hydrodynamic data
 
 	Returns:
-		depth_map (np.ndarray): (3,n,m) array of the depth map (TODO: CHECK THIS IS CORRECT?)
+		depth_area (np.ndarray): (n,m) array of the depths within the bbox
+		depth_func : interpolation function that outputs the depth within
+					 the array [+-width/2, +-height/2]
+
+	TODO: plot the output of this and check it looks correct	
 	'''
-	topo_depths = netcdf.NetCDFFile(depth_file)
-	lat_coords = topo_depths.variables['lat'][:].copy()
-	lon_coords = topo_depths.variables['lon'][:].copy()
+	topo_depths = netcdf.NetCDFFile(topo_file)
+	lat = topo_depths.variables['lat'][:].copy()
+	lon = topo_depths.variables['lon'][:].copy()
 	depths = topo_depths.variables['Band1'][:].copy()
 	topo_depths.close()
 
-	width = haversine(bbox[0,0], bbox[0,1], bbox[1,0], bbox[1,1])
-	height = haversine(bbox[1,0], bbox[1,1], bbox[2,0], bbox[2,1])
+	min_lat = min(bbox[:, 0])
+	max_lat = max(bbox[:, 0])
+	min_lon = min(bbox[:, 1])
+	max_lon = max(bbox[:, 1])
 
+	##Finding the lon bounds is different cause the values are negative
+	lon_idx = find_bound_idx(lon, [min_lon, max_lon])
+	lon_idx = np.sort(lon.shape[0]-1-lon_idx)
+	lat_idx = find_bound_idx(lat, [min_lat, max_lat])
 
+	##pull that data from the depth file:
+	depths_area = depths[lat_idx[0]:lat_idx[-1]+1, lon_idx[0]:lon_idx[-1]+1]
 
+	##interpolation things:
+	width = haversine(bbox[0,0], bbox[0,1], bbox[1,0], bbox[1,1])*1000
+	height = haversine(bbox[1,0], bbox[1,1], bbox[2,0], bbox[2,1])*1000
 
-	pass
+	x = np.linspace(-width/2, width/2, depths_area.shape[0], endpoint = True)
+	y = np.linspace(-height/2, height/2, depths_area.shape[1], endpoint = True)
+
+	depths_func = RegularGridInterpolator((x, y), depths_area)
+
+	return depths_area, depths_func
 
 
 def get_current_block(bbox, current_file):
@@ -224,120 +212,59 @@ def get_current_block(bbox, current_file):
 		current_file (string): path to netcdf file containing the hydrodynamic data
 
 	Returns:
-		u (np.ndarray): (n,m) array of all the currents in the u direction
-		v (np.ndarray): (n,m) array of all the currents in the v direction
+		u_area (np.ndarray): (n,m) array of the currents in the u direction within the bbox
+		v_area (np.ndarray): (n,m) array of the currents in the v direction within the bbox
+		u_func : interpolation function that outputs the u value at the specified array[depth, x, y] point
+		v_func : interpolation function that outputs the v value at the specified array[depth, x, y] point
+
 	'''
 	all_currents = netcdf.NetCDFFile(current_file)
-
-	lat = all_currents.variables['lat'][:].copy()
 	lon = all_currents.variables['lon'][:].copy()
 	lon -= 360
+	lat = all_currents.variables['lat'][:].copy()
 	u = all_currents.variables['u'][:].copy()
 	v = all_currents.variables['v'][:].copy()
+	depths = all_currents.variables['depth'][:].copy()
 	all_currents.close()
 
 	##find the bounds
-	min_lat = min(bbox[:, 0])
-	max_lat = max(bbox[:, 0])
 	min_lon = min(bbox[:, 1])
 	max_lon = max(bbox[:, 1])
+	min_lat = min(bbox[:, 0])
+	max_lat = max(bbox[:, 0])
 
-	# ##Finding the lon bounds is different cause the values are negative
-	# ##Not important todo: figure out a consistent method to pull both lat/lon
-	# lon_start = max(np.where(lon<=min_lon)[0])
-	# lon_end = min(np.where(lon>=max_lon)[0])
-	# lat_start = np.searchsorted(lat, min_lat, 'left')
-	# lat_end = np.searchsorted(lat, max_lat, 'right')
-
-	# if lon_start == lon_end:
-	# 	lon_idx = np.arange(lon_start-1, lon_end+1)
-	# else:
-	# 	lon_idx = np.array((lon_start, lon_end))
-	# if lat_start == lat_end:
-	# 	lat_idx = np.arange(lat_start-1, lat_end+1)
-	# else:
-	# 	lat_idx = np.array((lat_start, lat_end))
-
-	# print ("lon_idx: ", lon_idx)
-	# print ("lat_idx: ", lat_idx)
-
-	##This is the unimportant todo fix
-	##Processing cause the data is negative
-	##Not sure this was worth the trouble
+	##Finding the lon bounds is different cause the values are negative
 	lon_idx = find_bound_idx(lon, [min_lon, max_lon])
 	lon_idx = np.sort(lon.shape[0]-1-lon_idx)
-
 	lat_idx = find_bound_idx(lat, [min_lat, max_lat])
 
-	print ("lon_idx: ", lon_idx)
-	print ("lat_idx: ", lat_idx)
+	X = lon[lon_idx[0]:lon_idx[-1]+1]
+	Y = lat[lat_idx[0]:lat_idx[-1]+1]
 
 	##pull those coordinates from the current file
-	# lon_area = lon[lon_idx]
-	# lat_area = lat[lat_idx]
+	u_area = (u[0, :, lat_idx[0]:lat_idx[-1]+1, lon_idx[0]:lon_idx[-1]+1])
+	v_area = (v[0, :, lat_idx[0]:lat_idx[-1]+1, lon_idx[0]:lon_idx[-1]+1])
 
-	print ('lon shape: ', lon.shape)
-	print ('lat shape: ', lat.shape)
-	print ("u shape: ", u.shape)
+	##Filter out curernts that are greater than -9999:
+	u_area[u_area<-800] = 0.0
+	v_area[v_area<-800] = 0.0
 
-	u_area = u[0, :, lat_idx[0]:lat_idx[-1], lon_idx[0]:lon_idx[-1]]
-	v_area = v[0, :, lat_idx[0]:lat_idx[-1], lon_idx[0]:lon_idx[-1]]
-
-	print ('u_area shape: ', u_area.shape)
-	print ("v_area shaep: ", v_area.shape)
-
-
-	##Split into a different method
+	uv_area = np.hypot(u_area, v_area)
 
 	##This is all interpolation things
 	##allocate arrays for width, height
 	width = haversine(bbox[0,0], bbox[0,1], bbox[1,0], bbox[1,1])*1000
 	height = haversine(bbox[1,0], bbox[1,1], bbox[2,0], bbox[2,1])*1000
 	
-	uaxis = np.arange(0, u_area.shape[0])
-	vaxis = np.arange(0, v_area.shape[0])
+	##create grid with lat-lon coordinates
+	x = np.linspace(-width/2, width/2, u_area.shape[1], endpoint = True)
+	y = np.linspace(-height/2, height/2, u_area.shape[2], endpoint = True)
 
-	print ('width: ', width)
-	print ('uaxis: ', uaxis)
+	u_func = RegularGridInterpolator((depths, x, y), u_area)
+	v_func = RegularGridInterpolator((depths, x, y), v_area)
+	uv_func = RegularGridInterpolator((depths, x, y), uv_area)
 
-	xcoords = np.linspace(-width/2, width/2, width)
-	ycoords = np.linspace(-height/2, height/2, height)
-	
-	pdb.set_trace()
-
-	print ('xcoords: ', xcoords)
-	print ('ycoords: ', ycoords)
-
-	u_func = griddata(uaxis, u_area, ())
-	v_func = interp1d(vaxis, v_area)
-
-	##interpolate values across those arrays
-	##u = interpolate
-	##v = interpolate
-
-	'''
-	To test:
-		pull the same coordinate from the smaller file
-		pull the same coordinate from the larger current file
-		Should be the same value
-
-		maybe test how long it takes to run it?
-	'''
-
-
-	pass
-
-
-def interpolate_pts(boundary1, boundary2, data_idx, boundary1_data, boundary2_data):
-	'''
-	Input:
-		boundary1 (float): any
-
-	'''
-
-
-	print "FOUND THIS METHOD!"
-	pass
+	return u_area, v_area, uv_area, u_func, v_func, uv_func
 
 
 rospack = rospkg.RosPack()
@@ -345,10 +272,13 @@ trash_finder_path = rospack.get_path("trash_finder")
 data_path = rospack.get_path("data_files")
 
 config = SafeConfigParser()
-config.read(trash_finder_path+'/scripts/demo_configs.ini')
+config.read(trash_finder_path + '/scripts/demo_configs.ini')
 
 currents1 = config.get('CURRENTS', 'current_file')
-currents_path = data_path+'/'+currents1
+currents_path = data_path + '/' + currents1
+
+topo = config.get('TOPO', 'topo_file')
+topo_path = data_path + '/' + topo
 
 places = np.array([
 	[config.get('COORDS', 'loc1_lat'), config.get('COORDS', 'loc1_lon')],
@@ -358,3 +288,4 @@ places = np.array([
 	], dtype=float)
 print ("PLACES: ", places)
 get_current_block(places, currents_path)
+# get_depth_block(places, topo_path)
