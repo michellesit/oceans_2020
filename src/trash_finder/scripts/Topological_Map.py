ab55f15b47e79ad616df0436d5e75e121656139c
michellesit <mcsit@eng.ucsd.edu>
2020-05-06 18:46:03 -0700

    WIP need to be updated with pickle config files

--- src/trash_finder/scripts/Topological_Map.py
+++ src/trash_finder/scripts/Topological_Map.py
@@ -1,4 +1,5 @@
 import numpy as np
+import rospkg
 from scipy.io import netcdf
 import matplotlib.pyplot as plt
 
@@ -6,29 +7,31 @@ import matplotlib.pyplot as plt
 ##read in the depth map
 ##Get the coordinates of the area of interest
 
-class Topological_Map():
+def get_map_contours(self):
+	rospack = rospkg.RosPack()
+	data_path = rospack.get_path("data_files")
 
-	def get_map_contours(self):
-		socal = netcdf.NetCDFFile('./southern_calif_crm_v1.nc')
-		xog = socal.variables['x'][:]
-		yog = socal.variables['y'][:]
-		zog = socal.variables['z'][:]
+	socal = netcdf.NetCDFFile(data_path+'/southern_calif_crm_v1.nc')
+	xog = np.copy(socal.variables['x'][:])
+	yog = np.copy(socal.variables['y'][:])
+	zog = np.copy(socal.variables['z'][:])
+	socal.close()
 
-		xfilter = np.argwhere((xog <= -117.343) & (xog >= -117.543))
-		xclip = xog[xfilter].flatten()
+	xfilter = np.argwhere((xog <= -117.343) & (xog >= -117.543))
+	xclip = xog[xfilter].flatten()
 
-		yfilter = np.argwhere((yog>=32.66) & (yog<= 32.838))
-		yclip = yog[yfilter].flatten()
+	yfilter = np.argwhere((yog >= 32.66) & (yog <= 32.838))
+	yclip = yog[yfilter].flatten()
 
-		x,y = np.meshgrid(xclip, yclip)
-		z1 = zog[yfilter[0]:yfilter[-1]+1, xfilter[0]:xfilter[-1]+1]
+	x,y = np.meshgrid(xclip, yclip)
+	z1 = zog[yfilter[0]:yfilter[-1]+1, xfilter[0]:xfilter[-1]+1]
 
-		fig, ax = plt.subplots(figsize=(15,15))
-		CS = ax.contour(x,y,z1)
-		ax.clabel(CS, inline=1, fontsize=10)
-		# plt.show()
+	fig, ax = plt.subplots(figsize=(15,15))
+	CS = ax.contour(x,y,z1)
+	ax.clabel(CS, inline=1, fontsize=10)
+	# plt.show()
 
-		contour = CS.collections
-		return contour
-		
-		# contour.get_paths()[0].vertices
+	contour = CS.collections
+	return contour
+	
+	# contour.get_paths()[0].vertices
