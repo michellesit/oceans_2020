727d50dd9b91b5aaf5c2fb270736fe196fc14a64
Michelle Sit <mcsit@eng.ucsd.edu>
2020-07-02 04:34:03 -0700

    NEED TO TEST. Changed numpy version to 1.13 from 1.11

--- oceans_docker/install_general_env.sh
+++ oceans_docker/install_general_env.sh
@@ -18,8 +18,10 @@ apt-get install --yes python-pip \
 	python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose \
 	python-catkin-tools
 
+pip install numpy==1.13.0
 pip install numpy-stl Shapely
 
 ## Optional python3 packages:
 apt-get install --yes python3-numpy python3-scipy python3-matplotlib python3-pip
+pip2 install numpy==1.13.0
 pip3 install numpy-stl
